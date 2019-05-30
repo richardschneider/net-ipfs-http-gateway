31bed45be4ca2d1b2e6e98945fc91e331077fb14
Richard Schneider <makaretu@gmail.com>
2019-05-30 19:21:55 +1200

    feat: initial bits
    Mainly copied from the Ipfs Server in the IpfsEngine project.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # net-ipfs-http-gateway
 Allows HTTP access to IPFS files and directories
+
+## Related Projects
+
+- [IPFS Core](https://github.com/richardschneider/net-ipfs-core) - The core objects and interfaces of the Inter Planetary File System
+- [IPFS Engine](https://github.com/richardschneider/net-ipfs-engine) - Implements the Core API.
+- [IPFS HTTP Client](https://github.com/richardschneider/net-ipfs-http-client) - A .Net client library for the IPFS HTTP API.
+- [Peer Talk](https://github.com/richardschneider/peer-talk) - Peer to peer communication.
+
+## License
+Copyright © 2019 Richard Schneider (makaretu@gmail.com)
+
+This library is licensed under the [MIT](http://www.opensource.org/licenses/mit-license.php "Read more about the MIT license form") license. Refer to the [LICENSE](https://github.com/richardschneider/net-ipfs-http-gateway/blob/master/LICENSE) file for more information.
+
+<a href="https://www.buymeacoffee.com/kmXOxKJ4E" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/yellow_img.png" alt="Buy Me A Coffee" style="height: auto !important;width: auto !important;" ></a>
---
+++ src/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Ipfs.Engine;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Ipfs.HttpGateway
+{
+    public class Program
+    {
+        /// <summary>
+        ///   The IPFS Core API engine.
+        /// </summary>
+        public static IpfsEngine IpfsEngine;
+        const string passphrase = "this is not a secure pass phrase";
+
+        public static void Main(string[] args)
+        {
+            IpfsEngine = new IpfsEngine(passphrase.ToCharArray());
+            IpfsEngine.Start();
+
+            try
+            {
+                CreateWebHostBuilder(args)
+                    .Build()
+                    .Run();
+            }
+            finally
+            {
+                IpfsEngine?.Stop();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options => {
+                    options.Listen(IPAddress.Loopback, 8080); //HTTP port
+                })
+                .UseStartup<Startup>();
+    }
+}
